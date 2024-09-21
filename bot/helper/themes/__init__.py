308f39943d6269af3c8aafbcda7660321011796e
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2024-09-22 06:31:45 +0700

    Update __init__.py

--- bot/helper/themes/__init__.py
+++ bot/helper/themes/__init__.py
@@ -5,7 +5,7 @@ from random import choice as rchoice
 from bot import config_dict, LOGGER
 from bot.helper.themes import wzml_minimal
 
-AVL_THEMES = {{"minimal": wzml_minimal, "syuhadak": wzml_syuhadak, "terbaru": wzml_terbaru}}
+AVL_THEMES = {"minimal": wzml_minimal, "syuhadak": wzml_syuhadak, "terbaru": wzml_terbaru}
 for theme in listdir('bot/helper/themes'):
     if theme.startswith('wzml_') and theme.endswith('.py'):
         AVL_THEMES[theme[5:-3]] = import_module(f'bot.helper.themes.{theme[:-3]}')
