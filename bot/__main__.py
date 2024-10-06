052501236cc3dfdd25d6aca8fb7b8f2cb5a85693
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2024-10-06 15:10:32 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -193,12 +193,12 @@ async def restart_notification():
         if notifier_dict := await DbManger().get_incomplete_tasks():
             for cid, data in notifier_dict.items():
                 msg = BotTheme('RESTART_SUCCESS', time=now.strftime('%I:%M:%S %p'), date=now.strftime('%d/%m/%y'), timz=config_dict['TIMEZONE'], version=get_version()) if cid == chat_id else BotTheme('RESTARTED')
-                msg += "\n\n⌬ <b><i>Incomplete Tasks!</i></b>"
+                msg += "\n\n💥<b><i>Tugas gagal di selesaikan\nUlangi tugas anda</i></b>"
                 for tag, links in data.items():
-                    msg += f"\n➲ <b>User:</b> {tag}\n┖ <b>Tasks:</b>"
+                    msg += f"\n<b>Pengguna:</b> {tag}\n <b>Tugas:</b>"
                     for index, link in enumerate(links, start=1):
                         msg_link, source = next(iter(link.items()))
-                        msg += f" {index}. <a href='{source}'>S</a> ->  <a href='{msg_link}'>L</a> |"
+                        msg += f" {index}. <a href='{source}'>Sumber Tautan</a> ->  <a href='{msg_link}'>Link Chat</a> |"
                         if len(msg.encode()) > 4000:
                             await send_incompelete_task_message(cid, msg)
                             msg = ''
