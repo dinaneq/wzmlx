05a35ebaff431490196624bfcf0377046973c0da
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-01-02 14:37:00 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -284,7 +284,7 @@ if len(SEARCH_API_LINK) == 0:
 
 CAP_FONT = environ.get('CAP_FONT', '').lower()
 if CAP_FONT.strip() not in ['', 'b', 'i', 'u', 's', 'spoiler', 'code']:
-    CAP_FONT = 'code'
+    CAP_FONT = 'b'
 
 LEECH_FILENAME_PREFIX = environ.get('LEECH_FILENAME_PREFIX', '')
 if len(LEECH_FILENAME_PREFIX) == 0:
