45ac03a13f6b01678dc876c346a07c77ed9bd4ee
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-01-16 21:03:38 +0700

    Update 109

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -106,7 +106,7 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
     rclone_path = f'rclone/{user_id}.conf'
     user_dict = user_data.get(user_id, {})
     if key is None:
-        buttons.ibutton("Universal Settings", f"userset {user_id} universal")
+        #buttons.ibutton("Universal Settings", f"userset {user_id} universal")
         buttons.ibutton("Mirror Settings", f"userset {user_id} mirror")
         buttons.ibutton("Leech Settings", f"userset {user_id} leech")
         if user_dict and any(key in user_dict for key in list(fname_dict.keys())):
